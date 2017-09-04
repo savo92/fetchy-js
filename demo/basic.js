f0aa284c3b0dae2f7c48fdc73bf070841d9c30f0
lorenzo.savini <lorenzo.savini92@gmail.com>
2017-09-04 21:48:31 +0200

    too much logs

--- demo/basic.js
+++ demo/basic.js
@@ -1,6 +1,5 @@
 const fetch = require("../dist/index").fetch;
 
-console.log(fetch);
 fetch("https://something.ext", {}, { middlewares: [], retry: true })
     .then((r) => {
         r.text().then((t) => console.log(t));
