9a9fd771973a4a7886d25aaccaece0f23524dabd
lorenzo.savini <lorenzo.savini92@gmail.com>
2018-05-28 12:37:34 +0200

    Middleware for Raven client integration (Sentry)

---
+++ src/middlewares/raven.ts
@@ -0,0 +1,69 @@
+import { FetchyError } from "../utils/error";
+import {
+    FetchyMiddleware,
+    IFetchyMiddlewareConfig,
+} from "./base";
+
+export interface IFetchyRavenMiddlewareConfig extends IFetchyMiddlewareConfig {
+    categories?: {
+        error: string;
+        response: string;
+    };
+    // tslint:disable-next-line no-any  @TODO properly declare me, please
+    ravenClient: any;
+}
+
+export class IFetchyRavenMiddleware extends FetchyMiddleware {
+    protected config: IFetchyRavenMiddlewareConfig;
+
+    private errorCategory: string = "fetchy-js_response_captured";
+    private responseCategory: string = "fetchy-js_error_captured";
+
+    public constructor(config: IFetchyRavenMiddlewareConfig, next) {
+        super(config, next);
+
+        if (config.categories !== undefined) {
+            this.errorCategory = config.categories.error;
+            this.responseCategory = config.categories.response;
+        }
+    }
+
+    public async processResponse(promise0: Promise<Response>): Promise<Response> {
+        return promise0
+            .catch((e: FetchyError) => {
+                if (e.hasErrors()) {
+                    e.errors.forEach((error) =>
+                        this.captureErrorBreadcrumb(error));
+                }
+                if (e.hasResponses()) {
+                    e.responses.forEach((response) =>
+                        this.captureResponseBreadcrumb(response.clone()));
+                }
+
+                throw e;
+            });
+    }
+
+    private captureErrorBreadcrumb(error: Error): void {
+        this.config.ravenClient.captureBreadcrumb({
+            category: this.errorCategory,
+            data: {
+                message: error.message,
+                name: error.name,
+            },
+            message: "Failed response",
+        });
+    }
+
+    private captureResponseBreadcrumb(response: Response): void {
+        this.config.ravenClient.captureBreadcrumb({
+            category: this.responseCategory,
+            data: {
+                body: response.body,
+                status: response.status,
+            },
+            message: "Error occurred",
+        });
+    }
+
+}
